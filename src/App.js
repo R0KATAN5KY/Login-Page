bdf691e9d089c6a415e96c4189997a8ba25fffbb
Shelby Ozuna <shelbytheoz@gmail.com>
2022-12-06 23:30:41 -0600

    added files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar"
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <>
+    <div>
+      <Navbar />
+    </div>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">Week 13 Login Page</a>
+        <ul>
+            <li>
+                <a href="/home">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+    </nav>
+}
